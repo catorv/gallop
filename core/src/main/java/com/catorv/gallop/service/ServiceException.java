471af07b1a118affb11b3819b599c3c32108c584
Cator V <hi@catorv.com>
2016-10-23 14:38:34 +0800

    新增 ServiceException.throwException(Exception e, CodeMessagePair... pairs)

--- core/src/main/java/com/catorv/gallop/service/ServiceException.java
+++ core/src/main/java/com/catorv/gallop/service/ServiceException.java
@@ -1,5 +1,9 @@
 package com.catorv.gallop.service;
 
+import com.google.common.base.Preconditions;
+
+import javax.annotation.Nullable;
+
 /**
  * Service Exception
  * Created by cator on 8/15/16.
@@ -10,20 +14,21 @@ package com.catorv.gallop.service;
 })
 public abstract class ServiceException extends Exception {
 
-	public static final ServiceException OK                 = new E(0, "OK");
-	public static final ServiceException UNKNOWN            = new E(1, "未知错误");
-	public static final ServiceException PERMISSION_DENIED  = new E(2, "您无权进行此操作");
-	public static final ServiceException OPERATION_FAILED   = new E(3, "操作失败");
-	public static final ServiceException ILLEGAL_PARAMETER  = new E(4, "非法参数");
-	public static final ServiceException MISSING_PARAMETER  = new E(5, "缺少参数");
-	public static final ServiceException BAD_DATA           = new E(6, "数据错误");
-	public static final ServiceException MISSING_DATA       = new E(7, "数据错误");
-	public static final ServiceException ENTITY_NOT_FOUND   = new E(8, "未找到数据");
-	public static final ServiceException DUPLICATE_ENTITY   = new E(9, "数据重复");
-	public static final ServiceException AUTH_FAILED        = new E(10, "认证失败");
-	public static final ServiceException INVALID_SESSION    = new E(11, "无效的Session");
+	public static final ServiceException OK = new E(0, "OK");
+	public static final ServiceException UNKNOWN = new E(1000, "未知错误");
+	public static final ServiceException PERMISSION_DENIED = new E(1001, "权限禁止");
+	public static final ServiceException OPERATION_FAILED = new E(1002, "操作失败");
+	public static final ServiceException OPERATION_TIMEOUT = new E(1003, "操作超时");
+	public static final ServiceException ILLEGAL_PARAMETER = new E(1010, "非法参数");
+	public static final ServiceException MISSING_PARAMETER = new E(1011, "缺少参数");
+	public static final ServiceException BAD_DATA = new E(1020, "数据错误");
+	public static final ServiceException MISSING_DATA = new E(1021, "数据错误");
+	public static final ServiceException ENTITY_NOT_FOUND = new E(1022, "未找到数据");
+	public static final ServiceException DUPLICATE_ENTITY = new E(1023, "数据重复");
+	public static final ServiceException AUTH_FAILED = new E(1090, "认证失败");
+	public static final ServiceException INVALID_SESSION = new E(1091, "无效的Session");
 
-	private int code = -1;
+	private int code = 1000;
 
 	public ServiceException() {
 	}
@@ -62,6 +67,44 @@ public abstract class ServiceException extends Exception {
 		return new E(code, getMessage(), cause);
 	}
 
+	public static void throwException(Exception e, @Nullable CodeMessagePair... pairs)
+			throws ServiceException {
+		if (e instanceof ServiceException) {
+			throwWithMessage((ServiceException) e, pairs);
+		}
+
+		if (e instanceof javax.persistence.EntityNotFoundException ||
+				e instanceof javax.persistence.NoResultException) {
+			throwWithMessage(ENTITY_NOT_FOUND.withCause(e), pairs);
+		}
+
+		if (e instanceof javax.persistence.EntityExistsException) {
+			throwWithMessage(DUPLICATE_ENTITY.withCause(e), pairs);
+		}
+
+		if (e instanceof javax.persistence.LockTimeoutException ||
+				e instanceof javax.persistence.QueryTimeoutException) {
+			throwWithMessage(OPERATION_TIMEOUT.withCause(e), pairs);
+		}
+
+		throwWithMessage(OPERATION_FAILED.withCause(e), pairs);
+	}
+
+	private static void throwWithMessage(ServiceException exception,
+	                                     @Nullable CodeMessagePair[] pairs)
+			throws ServiceException {
+
+		if (pairs != null) {
+			for (CodeMessagePair pair : pairs) {
+				if (pair != null && exception.getCode() == pair.code) {
+					throw exception.withMessage(String.valueOf(pair.message));
+				}
+			}
+		}
+
+		throw exception;
+	}
+
 	private static class E extends ServiceException {
 
 		public E(int code, String message) {
@@ -73,4 +116,22 @@ public abstract class ServiceException extends Exception {
 		}
 
 	}
+
+	public static class CodeMessagePair {
+
+		public final int code;
+		public final Object message;
+
+		public CodeMessagePair(int code, Object message) {
+			Preconditions.checkNotNull(message, "message is null");
+			this.code = code;
+			this.message = message;
+		}
+
+		public CodeMessagePair(ServiceException exception, Object message) {
+			this(exception.getCode(), message);
+		}
+
+	}
+
 }
