3224f4d77b798e1fe8b01200efebb699fdf2cd80
Cator V <hi@catorv.com>
2016-10-23 23:59:40 +0800

    重构 MediaTypeUTF8

--- rs/src/main/java/com/catorv/gallop/rs/MediaTypeUTF8.java
+++ rs/src/main/java/com/catorv/gallop/rs/MediaTypeUTF8.java
@@ -8,17 +8,31 @@ import javax.ws.rs.core.MediaType;
  */
 public class MediaTypeUTF8 extends MediaType {
 
-	public static final String WILDCARD = "*/*; charset=utf-8";
-	public static final String APPLICATION_XML = "application/xml; charset=utf-8";
-	public static final String APPLICATION_ATOM_XML = "application/atom+xml; charset=utf-8";
-	public static final String APPLICATION_XHTML_XML = "application/xhtml+xml; charset=utf-8";
-	public static final String APPLICATION_SVG_XML = "application/svg+xml; charset=utf-8";
-	public static final String APPLICATION_JSON = "application/json; charset=utf-8";
-	public static final String APPLICATION_FORM_URLENCODED = "application/x-www-form-urlencoded; charset=utf-8";
-	public static final String MULTIPART_FORM_DATA = "multipart/form-data; charset=utf-8";
-	public static final String APPLICATION_OCTET_STREAM = "application/octet-stream; charset=utf-8";
-	public static final String TEXT_PLAIN = "text/plain; charset=utf-8";
-	public static final String TEXT_XML = "text/xml; charset=utf-8";
-	public static final String TEXT_HTML = "text/html; charset=utf-8";
+	private static final String CHARSET = "utf-8";
+
+	public static final String WILDCARD = MediaType.WILDCARD + "; " + MediaType.CHARSET_PARAMETER + "=" + CHARSET;
+	public static final MediaType WILDCARD_TYPE = MediaType.WILDCARD_TYPE.withCharset(CHARSET);
+	public static final String APPLICATION_XML = MediaType.APPLICATION_XML + "; " + MediaType.CHARSET_PARAMETER + "=" + CHARSET;
+	public static final MediaType APPLICATION_XML_TYPE = MediaType.APPLICATION_XML_TYPE.withCharset(CHARSET);
+	public static final String APPLICATION_ATOM_XML = MediaType.APPLICATION_ATOM_XML + "; " + MediaType.CHARSET_PARAMETER + "=" + CHARSET;
+	public static final MediaType APPLICATION_ATOM_XML_TYPE = MediaType.APPLICATION_ATOM_XML_TYPE.withCharset(CHARSET);
+	public static final String APPLICATION_XHTML_XML = MediaType.APPLICATION_XHTML_XML + "; " + MediaType.CHARSET_PARAMETER + "=" + CHARSET;
+	public static final MediaType APPLICATION_XHTML_XML_TYPE = MediaType.APPLICATION_XHTML_XML_TYPE.withCharset(CHARSET);
+	public static final String APPLICATION_SVG_XML = MediaType.APPLICATION_SVG_XML + "; " + MediaType.CHARSET_PARAMETER + "=" + CHARSET;
+	public static final MediaType APPLICATION_SVG_XML_TYPE = MediaType.APPLICATION_SVG_XML_TYPE.withCharset(CHARSET);
+	public static final String APPLICATION_JSON = MediaType.APPLICATION_JSON + "; " + MediaType.CHARSET_PARAMETER + "=" + CHARSET;
+	public static final MediaType APPLICATION_JSON_TYPE = MediaType.APPLICATION_JSON_TYPE.withCharset(CHARSET);
+	public static final String APPLICATION_FORM_URLENCODED = MediaType.APPLICATION_FORM_URLENCODED + "; " + MediaType.CHARSET_PARAMETER + "=" + CHARSET;
+	public static final MediaType APPLICATION_FORM_URLENCODED_TYPE = MediaType.APPLICATION_FORM_URLENCODED_TYPE.withCharset(CHARSET);
+	public static final String MULTIPART_FORM_DATA = MediaType.MULTIPART_FORM_DATA + "; " + MediaType.CHARSET_PARAMETER + "=" + CHARSET;
+	public static final MediaType MULTIPART_FORM_DATA_TYPE = MediaType.MULTIPART_FORM_DATA_TYPE.withCharset(CHARSET);
+	public static final String APPLICATION_OCTET_STREAM = MediaType.APPLICATION_OCTET_STREAM + "; " + MediaType.CHARSET_PARAMETER + "=" + CHARSET;
+	public static final MediaType APPLICATION_OCTET_STREAM_TYPE = MediaType.APPLICATION_OCTET_STREAM_TYPE.withCharset(CHARSET);
+	public static final String TEXT_PLAIN = MediaType.TEXT_PLAIN + "; " + MediaType.CHARSET_PARAMETER + "=" + CHARSET;
+	public static final MediaType TEXT_PLAIN_TYPE = MediaType.TEXT_PLAIN_TYPE.withCharset(CHARSET);
+	public static final String TEXT_XML = MediaType.TEXT_XML + "; " + MediaType.CHARSET_PARAMETER + "=" + CHARSET;
+	public static final MediaType TEXT_XML_TYPE = MediaType.TEXT_XML_TYPE.withCharset(CHARSET);
+	public static final String TEXT_HTML = MediaType.TEXT_HTML + "; " + MediaType.CHARSET_PARAMETER + "=" + CHARSET;
+	public static final MediaType TEXT_HTML_TYPE = MediaType.TEXT_HTML_TYPE.withCharset(CHARSET);
 
 }
