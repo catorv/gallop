a4f1085f7f51118f95503167a3e80cd846be22d9
Cator V <hi@catorv.com>
2016-10-23 11:55:40 +0800

    TypeCast的部分方法添加默认值

--- core/src/main/java/com/catorv/gallop/util/TypeCast.java
+++ core/src/main/java/com/catorv/gallop/util/TypeCast.java
@@ -12,59 +12,99 @@ public class TypeCast {
 	/**
 	 * 字符串转换成double
 	 * @param string 字符串
-	 * @return 如果转换成功,返回double型数值,否则返回0.0
+	 * @param defaultValue 默认值
+	 * @return 如果转换成功,返回 double 型数值,否则返回 defaultValue
 	 */
-	public static double doubleOf(String string) {
+	public static double doubleOf(String string, double defaultValue) {
 		try {
 			return Double.parseDouble(string);
 		} catch (Exception e) {
-			return 0.0;
+			return defaultValue;
+		}
+	}
+
+	/**
+	 * 字符串转换成double
+	 * @param string 字符串
+	 * @return 如果转换成功,返回 double 型数值,否则返回 0.0
+	 */
+	public static double doubleOf(String string) {
+		return doubleOf(string, 0.0);
+	}
+
+	/**
+	 * 字符串转换成float型
+	 * @param string 字符
+	 * @param defaultValue 默认值
+	 * @return 如果转换成功,返回float型字符,否则返回 defaultValue
+	 */
+	public static float floatOf(String string, float defaultValue) {
+		try {
+			return Float.parseFloat(string);
+		} catch (Exception e) {
+			return defaultValue;
 		}
 	}
 
 	/**
 	 * 字符串转换成float型
-	 * @param string 字符串
-	 * @return 如果转换成功,返回float型字符,否则返回0.0
+	 * @param string 字符
+	 * @return 如果转换成功,返回float型字符,否则返回 0.0
 	 */
 	public static float floatOf(String string) {
+		return floatOf(string, (float) 0.0);
+	}
+
+	/**
+	 * 字符串转换成long型
+	 * @param string 字符串
+	 * @param defaultValue 默认值
+	 * @return 如果转换成功,返回long型数值,否则返回 defaultValue
+	 */
+	public static long longOf(String string, long defaultValue) {
 		try {
-			return Float.parseFloat(string);
+			return ((Double) Double.parseDouble(string)).longValue();
 		} catch (Exception e) {
-			return (float) 0.0;
+			return defaultValue;
 		}
 	}
 
 	/**
 	 * 字符串转换成long型
 	 * @param string 字符串
-	 * @return 如果转换成功,返回long型数值,否则返回0
+	 * @return 如果转换成功,返回long型数值,否则返回 0L
 	 */
 	public static long longOf(String string) {
+		return longOf(string, 0L);
+	}
+
+	/**
+	 * 字符串转换成int型
+	 * @param string 字符串
+	 * @param defaultValue 默认值
+	 * @return 如果转换成功,返回int型数值,否则返回 defaultValue
+	 */
+	public static int intOf(String string, int defaultValue) {
 		try {
-			return ((Double) Double.parseDouble(string)).longValue();
+			return ((Double) Double.parseDouble(string)).intValue();
 		} catch (Exception e) {
-			return 0;
+			return defaultValue;
 		}
 	}
 
 	/**
 	 * 字符串转换成int型
 	 * @param string 字符串
-	 * @return 如果转换成功,返回int型数值,否则返回0
+	 * @return 如果转换成功,返回int型数值,否则返回 0
 	 */
 	public static int intOf(String string) {
-		try {
-			return ((Double) Double.parseDouble(string)).intValue();
-		} catch (Exception e) {
-			return 0;
-		}
+		return intOf(string, 0);
 	}
 
 	/**
 	 * 字符串转换成逻辑型
 	 * @param string 字符串
-	 * @return 如果字符串值为"true"或"false",则返回true,否则返回false
+	 * @return 如果字符串值为"true"或"yes",则返回true,否则返回false
 	 */
 	public static boolean booleanOf(String string) {
 		if (string == null) {
