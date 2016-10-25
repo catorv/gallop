558bb2847eefce21d401929f9bfc426609335104
Cator V <hi@catorv.com>
2016-10-25 17:20:03 +0800

    fix bug

--- data/src/main/java/com/catorv/gallop/database/jpa/HibernatePersistenceProviderResolver.java
+++ data/src/main/java/com/catorv/gallop/database/jpa/HibernatePersistenceProviderResolver.java
@@ -1,6 +1,6 @@
 package com.catorv.gallop.database.jpa;
 
-import com.catorv.gallop.database.jdbc.DruidDataSourceProvider;
+import com.catorv.gallop.database.jdbc.DataSourceProvider;
 import com.google.common.base.Preconditions;
 
 import javax.inject.Inject;
@@ -17,7 +17,7 @@ import java.util.Properties;
 public class HibernatePersistenceProviderResolver implements PersistenceProviderResolver {
 
 	@Inject
-	private DruidDataSourceProvider dataSourceProvider;
+	private DataSourceProvider dataSourceProvider;
 
 	@Inject
 	private Properties properties;
